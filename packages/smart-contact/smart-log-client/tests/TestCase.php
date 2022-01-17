91078e84e4cad0ee01a1143280cc21464679395e
Federico Mameli <federico_mameli@yahoo.com>
2022-01-17 12:39:34 +0100

    test: setup php unit & tests

---
+++ packages/smart-contact/smart-log-client/tests/TestCase.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace SmartContact\SmartLogClient\Tests;
+
+use SmartContact\SmartLogClient\SmartLogClientServiceProvider;
+
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+  public function setUp(): void
+  {
+    parent::setUp();
+    // additional setup
+  }
+
+  protected function getPackageProviders($app)
+  {
+    return [
+      SmartLogClientServiceProvider::class,
+    ];
+  }
+
+  protected function getEnvironmentSetUp($app)
+  {
+    // perform environment setup
+  }
+}
