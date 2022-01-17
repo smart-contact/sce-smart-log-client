ff25d34c7cfbd74d7d4d877a0b43981597b5d96b
Federico Mameli <federico_mameli@yahoo.com>
2022-01-17 12:02:11 +0100

    feat: setup project

---
+++ packages/smart-contact/smart-log-client/src/SmartLogClientServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace SmartContact\SmartLogClient;
+
+use Illuminate\Support\ServiceProvider;
+
+class SmartLogClientServiceProvider extends ServiceProvider{
+    public function boot()
+    {
+
+
+        // if($this->app->runningInConsole()){
+        //     $this->consoleBoot();
+        // }
+
+    }
+
+    // public function consoleBoot()
+    // {
+
+    // }
+
+
+    public function register()
+    {
+
+
+    }
+}
