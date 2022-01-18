67f53fb3ca5628421fd2cf2e8facb176238b91fa
Federico Mameli <federico_mameli@yahoo.com>
2022-01-18 18:22:34 +0100

    feat(exceptions): create SmartLogClientException

---
+++ src/Exceptions/SmartLogClientException.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace SmartContact\SmartLogClient\Exceptions;
+
+use Throwable;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Foundation\Exceptions\Handler;
+
+class SmartLogClientException extends Handler
+{
+
+    /**
+     * Report or log an exception.
+     *
+     * @param Throwable $e
+     * @return void
+     */
+    public function report(Throwable $e)
+    {
+        Log::emergency($e->getMessage(), [
+            'exception' => $e
+        ]);
+    }
+
+}
