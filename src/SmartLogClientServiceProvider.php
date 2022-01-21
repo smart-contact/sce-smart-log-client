918056e0540afe1d8c3c7d433247e8040658000a
Federico Mameli <federico_mameli@yahoo.com>
2022-01-21 16:22:07 +0100

    refactor(ServiceProvider): remove unused parts

--- src/SmartLogClientServiceProvider.php
+++ src/SmartLogClientServiceProvider.php
@@ -15,10 +15,6 @@ class SmartLogClientServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/logging.php', 'logging');
-        $this->publishes([
-            __DIR__.'/../config/smartlog.php' => config_path('smartlog.php')
-        ]);
 
         $this->app->singleton(SmartLogClient::class, function(){
             $httpClient = new Client([
@@ -37,22 +33,10 @@ class SmartLogClientServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-    }
-
-    /**
-     * Merge recursively (multi-dimensional array ver) the given configuration with the existing configuration.
-     *
-     * see: https://medium.com/@alkhwlani/why-not-use-build-in-function-array-merge-recursive-53ee8eee151c
-     * https://medium.com/@alkhwlani/why-not-use-build-in-function-array-merge-recursive-53ee8eee151c
-     *
-     * @param string $path
-     * @param string $key
-     * @return void
-     */
-    protected function mergeConfigFrom($path, $key)
-    {
-        $config = $this->app['config']->get($key, []);
-        $this->app['config']->set($key, array_merge_recursive($config, require $path));
+        if($this->app->runningInConsole()){
+            $this->publishes([
+                __DIR__.'/../config/smartlog.php' => config_path('smartlog.php')
+            ], 'smartlog-configs');
+        }
     }
 }
