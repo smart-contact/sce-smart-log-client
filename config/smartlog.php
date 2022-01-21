6780c442a3e97fda8008f6998bf4c3e32dadb9ae
Federico Mameli <federico_mameli@yahoo.com>
2022-01-21 16:19:23 +0100

    refactor(config): clean up config file

--- config/smartlog.php
+++ config/smartlog.php
@@ -2,7 +2,5 @@
 
 return [
   'apiBaseURL' => env('SMARTLOG_API_URL'),
-  'applicationName' => env('SMARTLOG_APP_NAME'),
-  'logLevel' => env('SMARTLOG_LEVEL', 'emergency'),
-  'notificationChannel' => env('SMARTLOG_NOTIFICATION_CHANNEL')
+  'applicationName' => env('SMARTLOG_APP_NAME')
 ];
